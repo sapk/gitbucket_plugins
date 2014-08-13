2ef03ce8510d52c35293d7eeb0d1f9607f329c73
Naoki Takezoe <takezoe@gmail.com>
2014-08-14 02:01:57 +0900

    Add Scala version of existing plugins

---
+++ export-issues/plugin.scala
@@ -0,0 +1,41 @@
+import plugin._
+
+val pluginDef = ScalaPlugin.define(id, version, author, url, description)
+
+pluginDef.addRepositoryAction("/issues/export", Security.Member()){ (request, response, repository) =>
+  db.select(s"""SELECT * FROM ISSUE WHERE
+    USER_NAME       = '${repository.owner}' AND
+    REPOSITORY_NAME = '${repository.name}'  AND
+    PULL_REQUEST    = false""").map { issue =>
+    Map(
+      "issueId"          -> issue("ISSUE_ID"),
+      "title"            -> issue("TITLE"),
+      "content"          -> issue("CONTENT"),
+      "openedUserName"   -> issue("OPENED_USER_NAME"),
+      "assignedUserName" -> issue("ASSIGNED_USER_NAME"),
+      "closed"           -> issue("CLOSED"),
+      "registeredDate"   -> issue("REGISTERED_DATE"),
+      "updatedDate"      -> issue("UPDATED_DATE"),
+      "comments"         -> db.select(s"""SELECT * FROM ISSUE_COMMENT WHERE
+        USER_NAME       = '${repository.owner}' AND
+        REPOSITORY_NAME = '${repository.name}'  AND
+        ISSUE_ID        = ${issue("ISSUE_ID")}
+        ORDER BY COMMENT_ID""").map { comment =>
+          Map(
+            "commentId"         -> comment("COMMENT_ID"),
+            "action"            -> comment("ACTION"),
+            "commentedUserName" -> comment("COMMENTED_USER_NAME"),
+            "content"           -> comment("CONTENT"),
+            "registeredDate"    -> comment("REGISTERED_DATE"),
+            "updatedDate"       -> comment("UPDATED_DATE")
+          )
+        }
+    )
+  }
+}
+
+pluginDef.addJavaScript((path: String) => path.endsWith("/issues"), """
+$("a:contains('New Issue')").before('<a class="btn btn-small" href="issues/export">Export as JSON</a>');
+""")
+
+PluginSystem.install(pluginDef)
---
+++ test/plugin.scala
@@ -0,0 +1,9 @@
+import plugin._
+
+val pluginDef = ScalaPlugin.define(id, version, author, url, description)
+
+pluginDef.addGlobalAction('/test'){ (request, response) =>
+  "<h1>This is Test Plug-In</h1>"
+}
+
+PluginSystem.install(pluginDef)
